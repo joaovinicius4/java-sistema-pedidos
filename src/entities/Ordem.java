872516599722dce53669fc36ba1a65651642f819
joaovinicius4 <joaoruas111@gmail.com>
2026-01-13 08:47:46 -0300

    Implementada classe OrdemItem

--- src/entities/Ordem.java
+++ src/entities/Ordem.java
@@ -1,5 +1,12 @@
 package entities;
 
-public class Ordem {
+import java.util.Date;
 
+import entities.enums.Order_Status;
+
+public class Ordem {
+	
+	private Date momento;
+	private Order_Status status;
+	
 }
--- src/entities/OrdemItem.java
+++ src/entities/OrdemItem.java
@@ -1,5 +1,44 @@
 package entities;
 
 public class OrdemItem {
+	
+	private Integer quantidade;
+	private Double preco;
+	private Produto produto;
 
+	public OrdemItem() {
+	}
+
+	public OrdemItem(Integer quantidade, Double preco, Produto produto) {
+		this.quantidade = quantidade;
+		this.preco = preco;
+		this.produto = produto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public Double subTotal() {
+		return preco * quantidade;
+	}
+
+	@Override
+	public String toString() {
+		return produto.getNome()
+				+ ", $"
+				+ String.format("%.2f", preco)
+				+ ", Quantity: "
+				+ quantidade
+				+ ", Subtotal: $"
+				+ String.format("%.2f", subTotal());
+	}
 }
