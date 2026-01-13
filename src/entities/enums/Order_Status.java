67dcf343b059478c1d06e38fa28fe46f293ee078
joaovinicius4 <joaoruas111@gmail.com>
2026-01-13 08:20:20 -0300

    Estrutura inicial do sistema de pedidos

---
+++ src/entities/enums/Order_Status.java
@@ -0,0 +1,8 @@
+package entities.enums;
+
+public enum Order_Status {
+	PAGAMENTO_PENDENTE,
+	PREPARANDO,
+	ENVIADO,
+	ENTREGUE;		
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Ordem_Pedidos {
+}
