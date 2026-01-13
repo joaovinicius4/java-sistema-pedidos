1f2878233efe3f2339e8c34f6561ea224334a17c
joaovinicius4 <joaoruas111@gmail.com>
2026-01-13 08:31:46 -0300

    Implementada classe Produto

--- src/entities/Produto.java
+++ src/entities/Produto.java
@@ -1,5 +1,23 @@
 package entities;
 
 public class Produto {
+	
+	private String nome;
+	private Double preco;
 
+	public Produto() {
+	}
+
+	public Produto(String nome, Double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
 }
